e92ba53e9258b5c60700f987e98e8dd5df070e36
makevoid <makevoid@bd542798-39b7-4839-a227-2adabaa986bb>
2011-08-22 15:42:51 +0000

    yes db config

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,21 +1,4 @@
 # execute rake db:seed
 path = File.expand_path "../../", __FILE__
 
-# step 1: recreate_tables
-require "#{path}/db/recreate_tables"
-
-
-########################################################
-puts "\nseeding..."
-
-cities = JSON.parse File.read("#{path}/db/cities.json")
-
-cities.each do |city|
-  city.symbolize_keys!
-  loc = Location.create lat: city[:lat], lng: city[:lng]
-  City.create name: city[:name], pts: 0, location_id: loc.id
-end
-
-
-puts "\ndone!"
-
+require "#{path}/db/refine"
