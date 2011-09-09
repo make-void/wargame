cd818178bda331f57a2b61886fb3a73811e382e7
cor3y <cor3y@bd542798-39b7-4839-a227-2adabaa986bb>
2011-09-09 07:31:33 +0000

    ??

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -2,7 +2,7 @@ class CitiesController < ApplicationController
   
   layout nil
   
-  def show
+  def show #Why not use the other method in LG::City witch return mutch more infos?
     data = LG::City.get params[:city]
     data = { error: { type: "not_found", message: "City not found (#{params[:city]})" } } unless data
     render json: data
